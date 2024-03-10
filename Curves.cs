b9bf291ee48cc3246f1e9394507e7e69546f5606
Swusan <bmoregarfield@gmail.com>
2024-03-09 19:34:09 -0500

    Deleted Unnecessary File

--- Curves.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace LethalFragGrenade;
-
-[CreateAssetMenu(fileName = "Curves", menuName = "ScriptableObjects/Curves")]
-public class Curves : ScriptableObject
-{
-    public AnimationCurve[] curves;
-}
